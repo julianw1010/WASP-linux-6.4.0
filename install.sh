3567fac732f8d0c3f2752f4ba8cdc6dfd1e73b61
Julian10 <Julian10@node0.pagetable.pagetable-pg0.clemson.cloudlab.us>
2026-01-12 16:57:43 +0000

    Updated install script

--- install.sh
+++ install.sh
@@ -1,5 +1,5 @@
-sudo rm /boot/vmlinuz-6.4.0*
-sudo rm /boot/initrd.img-6.4.0*
+sudo rm /boot/vmlinuz-6.4.0-wasp*
+sudo rm /boot/initrd.img-6.4.0-wasp*
 git pull
 git log -1 --pretty=format:"%h %s (%ci)"
 make -j$(nproc)
